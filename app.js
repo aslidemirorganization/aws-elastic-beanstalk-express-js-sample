f1de239cec8c792b417f8c371e184bad12f7b845
Asli Demir <asli@Aslis-MacBook-Air.local>
2021-01-21 21:54:27 +0300

    Full Pipeline Test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Yaptık Oldu'));
+app.get('/', (req, res) => res.send('Dedelerimizin mezar taşlarını okuyamayan bir nesil yetiştirdik......'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
